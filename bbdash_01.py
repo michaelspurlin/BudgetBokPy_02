0ece4e731919c184564c25794e33fb8838f8806d
Nilrups <michael@michaelspurlin.com>
2022-05-23 20:51:54 -0400

    Added radio buttons for dept/fund and table/chart

--- bbdash_01.py
+++ bbdash_01.py
@@ -14,7 +14,7 @@ df = pd.read_csv('Rock_City_FY2022_Budget_Data.csv')
 
 
 deptlist = df['dept_name'].unique()
-#epartment = st.selectbox("Select a department:", deptlist, index=0)
+
 
 #create by department list
 #df_group_dept = df.groupby(deptlist)
@@ -30,58 +30,63 @@ df_fund = df[['fund_name', 'amount']]
 df_fund_sum = df_fund.groupby(['fund_name']).sum(['amount'])
 df_fund_sum = df_fund_sum.sort_values(by='amount', ascending=False)
 
-#creatte for by fund
-#format inot currency
-#show all the rows
+#creating columns
+col1, col2 = st.columns((1,1))
+with col1:
+     choice = st.radio(
+    "Would you like to view by department name or by fund?",
+    ('By department', 'By fund'))
 
-
-#create the choice picklist
-# Using object notation
-
-choice = st.sidebar.selectbox("Select your view:", ("By department", "By fund"), index=0)
-
-#selecting
-if (choice == "By department"):
-    # display the data for the department choice
+if choice == 'By department':
+    df_1 = df_dept_sum
     st.header("By Department")
-
-    st.write("You selected department!!")
-    st.dataframe(df_dept_sum, height = 700)  #need dataframe to stet height or width
-    # Bart chart by all dpeartments- need plotly
-    fig = px.bar(df_dept_sum, x="amount", orientation='h')
-    st.write(fig)
-
-
 else:
+    df_1 = df_fund_sum
     st.header("By Fund")
+with col2:
 
-    st.write("You selected fund!!")
-    st.dataframe(df_fund_sum, height = 400)
-    fig = px.bar(df_fund_sum, x="amount", orientation='h')
+     view = st.radio(
+         "Would you like the table view or the chart view?",
+         ('table', 'chart'))
+
+
+# create choice (dept or fund or chart) radio buttons
+
+
+#create View (table or chart) radio buttons
+if view == 'table':
+    st.write('You selected the table view.')
+    st.dataframe(df_1, height=700)
+else:
+    st.write("You selected the chart view.")
+    fig = px.bar(df_1, x="amount", orientation='h')
     st.write(fig)
 
-#creating a batter table view of the data set using aggrid
-#put df into Agrid format
-#AgGrid(df)
-gb = GridOptionsBuilder.from_dataframe(df)
-gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
-gb.configure_side_bar() #Add a sidebar
-gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
-gridOptions = gb.build()
 
-grid_response = AgGrid(
-    df,
-    gridOptions=gridOptions,
-    data_return_mode='AS_INPUT',
-    update_mode='MODEL_CHANGED',
-    fit_columns_on_grid_load=False,
-    theme='blue', #Add theme color to the table
-    enable_enterprise_modules=True,
-    height=350,
-    width='100%',
-    reload_data=True
-)
+department = st.sidebar.selectbox("Select a department:", deptlist, index=0)
 
-data = grid_response['data']
-selected = grid_response['selected_rows']
-df2 = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df2
+# #creating a better table view of the data set using aggrid
+# #put df into Agrid format
+# #AgGrid(df)
+# gb = GridOptionsBuilder.from_dataframe(df)
+# gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+# gb.configure_side_bar() #Add a sidebar
+# gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+# gridOptions = gb.build()
+#
+# grid_response = AgGrid(
+#     df,
+#     gridOptions=gridOptions,
+#     data_return_mode='AS_INPUT',
+#     update_mode='MODEL_CHANGED',
+#     fit_columns_on_grid_load=False,
+#     theme='blue', #Add theme color to the table
+#     enable_enterprise_modules=True,
+#     height=350,
+#     width='100%',
+#     reload_data=True
+# )
+#
+# data = grid_response['data']
+# selected = grid_response['selected_rows']
+# df2 = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df2
